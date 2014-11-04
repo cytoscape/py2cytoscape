92bdd5935aad92e68cee1947d48858fa1716119a
Kozo Nishida <knishida@riken.jp>
2014-11-05 05:30:50 +0900

    add set_port method

--- py2cytoscape/data/table.py
+++ py2cytoscape/data/table.py
@@ -6,12 +6,16 @@ class Table(object):
     """The Table object provides methods for Cytoscape table manipulation"""
     def __init__(self):
         self.df = None
+        self.port = None
 
-    def push(self, port):
-        BASE_URL = "http://localhost:" + str(port) + "/v1/"
+    def push(self, target_table_name):
+        BASE_URL = "http://localhost:" + self.port + "/v1/"
         HEADERS = {'Content-Type': 'application/json'}
         jsontable = json.loads(self.df.to_json(orient="records"))
         print json.dumps(jsontable, indent=4)
+
+    def set_port(self, port):
+        self.port = str(port)
 
 def read(filepath):
     table = Table()
