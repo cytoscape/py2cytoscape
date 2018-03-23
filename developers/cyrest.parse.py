c1431f1d45b4274385b6d4fd15d8a92ac34ea435
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2018-03-23 10:57:52 +0100

    developers: swagger parser

---
+++ developers/cyrest.parse.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+"""
+Usage:
+
+./cyrest.parse.py cyparse.txt force-directed-cl
+"""
+
+import sys
+
+
+fin=sys.argv[1]
+
+args=[]
+with open(fin, "r") as f:
+    for line in f:
+        a=line.split(" ")[0]
+        args.append(a)
+if "network" in args:
+    net=True
+else:
+    net=False
+
+text=""
+for a in args:
+    text=text+a+"=None,"
+texta=text.rstrip(',')
+
+text="["
+for a in args:
+    text=text+"'"+a+"',"
+text=text.rstrip(",")
+text=text+"],["
+for a in args:
+    text=text+a+","
+text=text.rstrip(",")
+textb=text+"]"
+
+
+text=[]
+with open(fin, "r") as f:
+    for line in f:
+        l=line.rstrip("\n")
+        l=l.rstrip(",")
+        chunks, chunk_size = len(l), 68
+        l=[ l[i:i+chunk_size] for i in range(0, chunks, chunk_size) ]
+        l="\n\t\t\t".join(l)
+        l="\t\t:param "+l
+        text.append(l)
+textc="\n".join(text)
+
+fname=sys.argv[2]
+nfname=fname.replace(" ","_")
+nfname=nfname.replace("-","_")
+header='\tdef '+nfname+'(self,'+texta+',verbose=None):'
+textd=header+'\n\t\t"""\n'+textc+'\n\t\t"""'
+
+if net:
+    text="\n\t\tnetwork=check_network(self,network,verbose=verbose)"
+else:
+    text=""
+textf=text+'\n\t\tPARAMS=set_param('+textb+')\n\t\tresponse=api(url=self.__url+"/'+fname+'", PARAMS=PARAMS, method="POST", verbose=verbose)'
+
+print textd+textf
+
